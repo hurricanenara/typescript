8d2b5da380d425acf8a1d930265afb71f85156be
naracodes <naralee0124@gmail.com>
2021-05-10 20:56:26 -0400

    learn about core types of ts and how they behave with inference or explicit

--- app.ts
+++ app.ts
@@ -1,1 +1,1 @@
-console.log("My code");
+// union type in ts
--- types.ts
+++ types.ts
@@ -1,18 +1,21 @@
-// enum can be used
-var ADMIN = 0;
-var READ_ONLY = 1;
-var AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Nara",
-    age: 30,
-    hobbies: ["Gaming", "Lifting"],
-    role: Role.ADMIN
+// enum can be used instead of constant assignments
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// this is enum
+// if enum variable index has to start with other than 0, it can be assigned manually like AMIN = 5 (desired starting number)
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Nara",
+  age: 30,
+  hobbies: ["Gaming", "Lifting"],
+  role: Role.ADMIN,
 };
 // const person: {
 //   name: string;
@@ -25,15 +28,18 @@ var person = {
 //   hobbies: ["Gaming", "Lifting"],
 //   role: [2, "author"],
 // };
+
 // length is enforced when assigning or reassigning
 // person.role.push("admin"); // push is allowed in tuples so ts won't catch this
 // person.role[1] = 10;
-var favoriteActivities;
+
+let favoriteActivities: string[];
 favoriteActivities = ["Jogging"];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
 }
+
 // explicit type assignment
 // const person: {
 //   name: string;
@@ -42,4 +48,5 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
 //   name: "Nara",
 //   age: 30,
 // };
+
 console.log(person.name);
