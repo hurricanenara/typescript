805fc70d18a378c87bed7a3954f7d3ef290f41fd
naracodes <naralee0124@gmail.com>
2021-05-13 11:34:32 -0400

    public, private, readonly, and shorthand initialization

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,24 @@
 "use strict";
-const userName = "Nara";
-let age = 30;
-age = 29;
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.prototype.describe = function () {
+        console.log("Department " + this.id + ": " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var accounting = new Department("E1", "Engineering");
+accounting.addEmployee("Nara");
+accounting.addEmployee("Gammie");
+accounting.describe();
+accounting.printEmployeeInformation();
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,36 @@
-const userName = "Nara";
-let age = 30;
-age = 29;
+class Department {
+  //   private id: string;
+  //   private name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+    // shorthand initialization in params
+    // readonly prohibits it from changing
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department ${this.id}: ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department("E1", "Engineering");
+accounting.addEmployee("Nara");
+accounting.addEmployee("Gammie");
+
+// accounting.employees[2] = "P"; // avoid having more than one way to add
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "s", describe: accounting.describe };
+// accountingCopy.describe();
