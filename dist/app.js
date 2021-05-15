98396361499a8c6704eee2ec53595de996416bbb
naracodes <naralee0124@gmail.com>
2021-05-15 12:18:24 -0400

    interfaces and optional params

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,26 @@
 "use strict";
-var user1;
-user1 = {
-    name: "Nara",
-    age: 30,
-    greet: function (phrase) {
-        console.log(phrase + " " + this.name);
-    },
+var add;
+add = function (n1, n2) {
+    return n1 + n2;
 };
+var Person = (function () {
+    function Person(n) {
+        this.age = 30;
+        if (n) {
+            this.name = n;
+        }
+    }
+    Person.prototype.greet = function (phrase) {
+        if (this.name) {
+            console.log(phrase + " " + this.name);
+        }
+        else {
+            console.log("Hi!");
+        }
+    };
+    return Person;
+}());
+var user1;
+user1 = new Person();
 user1.greet("Hi, I'm");
+console.log(user1);
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,66 @@
-interface Person {
+//  --- interface ---
+
+// type vs interface function
+
+// type AddFunction = (a: number, b: number) => number;
+interface AddFunction {
+  // anonymous function in interface
+  (a: number, b: number): number; // : return type
+}
+let add: AddFunction;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+// interface can used by to only describe an object - but interface is clearer than type
+// interface like a module in js?
+interface Named {
+  //readonly can be added but not public or private
+  readonly name?: string;
+  outputName?: string; // optional property with a question mark
+}
+interface Greetable extends Named {
+  // can extend multiple interfaces
+  greet(phrase: string): void;
+}
+
+// combining interfaces with Named Greetable
+// separation of concerns is good
+
+// why not use type?
+/* 
+type Person = {
   name: string;
   age: number;
 
   greet(phrase: string): void;
-}
-
-let user1: Person;
-
-user1 = {
-  name: "Nara",
-  age: 30,
-  greet(phrase: string) {
-    console.log(phrase + " " + this.name);
-  },
 };
+*/
+
+class Person implements Greetable {
+  // can implement more than one interface separated by commas
+  name?: string;
+  age = 30;
+
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
+  }
+  greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
+  }
+}
+// interface forces/informs user1 must have a greet method
+let user1: Greetable;
+
+user1 = new Person();
+// user1.name = "not Nara"; // can't be done
 
 user1.greet("Hi, I'm");
+console.log(user1);
