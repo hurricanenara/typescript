a635cc56673de1db21bc49f379f0baa7887e86d5
naracodes <naralee0124@gmail.com>
2021-05-16 23:06:59 -0400

    type guard

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,19 @@ var e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("start date: " + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: "Nara", startDate: new Date() });
--- src/app.ts
+++ src/app.ts
@@ -29,3 +29,24 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("start date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation({ name: "Nara", startDate: new Date() });
