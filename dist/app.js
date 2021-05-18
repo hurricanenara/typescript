1109f9d5a345e616d2e13798c71f8423c2a33ab0
naracodes <naralee0124@gmail.com>
2021-05-18 16:22:27 -0400

    generic classes

--- dist/app.js
+++ dist/app.js
@@ -18,3 +18,26 @@ function extractAndConvert(obj, key) {
     return "Value: " + obj[key];
 }
 extractAndConvert({ name: "Nara" }, "name");
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Nara");
+textStorage.addItem("Seungyoon");
+textStorage.removeItem("Seungyoon");
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
