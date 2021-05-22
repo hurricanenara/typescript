07ff8320e5f79a9026347bfa88d059356cdf5fd0
naracodes <naralee0124@gmail.com>
2021-05-22 13:27:52 -0400

    get user input with configure and submit handler

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     // this.templateEl = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -15,8 +18,30 @@ class ProjectInput {
       true
     ); // true -> deep clone or not
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
